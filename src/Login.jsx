edb27356b86f4fbc4f833b24498efa67362f6b2b
Louktarn <aunnicha268@gmail.com>
2024-01-09 23:13:41 +0700

    fix css of the log in div

--- src/Login.jsx
+++ src/Login.jsx
@@ -7,27 +7,18 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission here
   };
 
   return (
-    <form onSubmit={handleSubmit} className="login-form">
-      <label htmlFor="username">Username:</label>
-      <input
-        type="text"
-        id="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        id="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Log In</button>
-    </form>
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" value={username} placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password"value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
   );
 };
 
